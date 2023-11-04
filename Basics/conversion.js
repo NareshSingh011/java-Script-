ab87250199791dc9c2c333049234b6b04f2e2cd7
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2023-11-04 18:41:55 +0000

    converion of datatypes

---
+++ Basics/conversion.js
@@ -0,0 +1,11 @@
+let score = "33"
+console.log(typeof score)
+
+// Datatypes Conversion 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+let logedIN = 1
+let boleanloggedIn = Boolean(logedIN)
+console.log(boleanloggedIn)
