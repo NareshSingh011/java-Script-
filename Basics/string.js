a51948e89968eea77f24e72cfe4c669a6431848b
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2023-11-05 18:41:32 +0000

    strings

---
+++ Basics/string.js
@@ -0,0 +1,9 @@
+const name  = {"Naresh Singh"}
+const repoCount = 30
+
+console.log (name+repoCount)
+console.log (`My name is ${name} and my repo is ${repoCount}`)
+
+const gameName = new String("Naresh Singh")
+console.log(gameName[0])
+console.log(gameName)
