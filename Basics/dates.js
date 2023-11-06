9a31318cdb95a76dd1028542bbdc8e6b68586169
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2023-11-06 07:44:57 +0000

    Dates in js

---
+++ Basics/dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log (myDate);
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+let createmyDate = new Date (2023,10,05)
+console.log(createmyDate.toDateString())
+console.log(createmyDate.toLocaleDateString())
