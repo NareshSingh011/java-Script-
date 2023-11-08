7ba014d8c4c73138fcd6e46d04ce3cc515510b56
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2023-11-08 06:40:51 +0000

    function in js

---
+++ Basics03/function.js
@@ -0,0 +1,30 @@
+// Rest operator 
+function createcart(...num1){
+    return num1
+}
+console.log(createcart(200,300,400))
+
+// Object
+
+const usser = {
+    name : "naresh Singh",
+    add : "Bajrang colony ashta",
+}
+
+function createcart(any){
+    console.log(`my name is ${any.name} and my add is ${any.add}`);
+}
+createcart({
+    name : "Deepak"
+})
+
+// Array 
+
+const myarray = [200,300,400,500]
+
+function second(Array){
+    return Array[1]
+}
+console.log(second(myarray))
+
+
