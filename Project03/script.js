73ef9768e19afd3c2040401d9a85d84d3a71e886
Naresh singh <naresh.s.ashta@gmail.com>
2023-11-29 15:09:37 +0530

    clock using java script

---
+++ Project03/script.js
@@ -0,0 +1,7 @@
+const clock = document.querySelector("#clock");
+
+
+setInterval(function() { //setInterval use for call a function repeatedly at a specified interval of time.
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
