379e397fcdf47b25496cfe4a297422d32bcb2566
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2024-02-29 22:02:55 +0000

    maths and numbers

---
+++ Basics/maths_nums.js
@@ -0,0 +1,29 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number (624.42)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(3));
+
+// const thousands = 1000000
+// console.log(thousands.toLocaleString('en-In'));
+
+// +++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++
+
+
+console.log(Math.abs(-30));
+console.log(Math.round(7.75))
+console.log(Math.floor(Math.random()*100 .toFixed(2)))
+
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random() * (max-min + 1))+min)
+
+// #concate 
+
+// const name = ["Naresh","satish","aryan","Ankush"]
+// const surname = ["Singh ","Singh","Mewada","Rajput"]
+// const join = name.concat(surname)
+// console.log(join)
