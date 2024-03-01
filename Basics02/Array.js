2a33842ad237f06dc2f43a02d0c942104c9e9fb8
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2024-03-01 19:13:58 +0000

    arrays in js

--- Basics02/Array.js
+++ Basics02/Array.js
@@ -1,40 +1,42 @@
-let myArr = [1,2,3,4,5]
-// myArr.push(8)
-// myArr.pop()
-// console.log(myArr)
+// let myArr = [1,2,3,4,5]
+// // myArr.push(8)
+// // myArr.pop()
+// // console.log(myArr)
 
-let myn1  = myArr.slice(0,3)
-console.log("A",myArr)
-console.log(myn1)
+// let myn1  = myArr.slice(0,3)
+// console.log("A",myArr)
+// console.log(myn1)
 
-let myn2 = myArr.splice(0,3)
-console.log("B",myArr)
-console.log(myn2)
+// let myn2 = myArr.splice(0,3)
+// console.log("B",myArr)
+// console.log(myn2)
 
-// Push
+// // Push
 
-let bolly_heros = ['salman','sharukh','ritik']
-let south_heros = ['vijay','dhanush','rajnikanth']
-// bolly_heros.push(south_heros)
-// console.log(bolly_heros)
+// let bolly_heros = ['salman','sharukh','ritik']
+// let south_heros = ['vijay','dhanush','rajnikanth']
+// // bolly_heros.push(south_heros)
+// // console.log(bolly_heros)
 
-//Concat 
+// //Concat 
 
-let allheros = bolly_heros.concat(south_heros)
-console.log(allheros);
+// let allheros = bolly_heros.concat(south_heros)
+// console.log(allheros);
 
-// Spread
+// // Spread
 
-let allnewheros = [...bolly_heros,...south_heros]
-console.log(allheros)
+// let allnewheros = [...bolly_heros,...south_heros]
+// console.log(allheros)
 
-// Flat Array 
-let another_array = [1,2,3,4,5,[2,2,3,4,5,[2,5,6,7,3],2,3,4]]
-let solve_Array = another_array.flat(Infinity)
-console.log(solve_Array);
+// // Flat Array 
+// let another_array = [1,2,3,4,5,[2,2,3,4,5,[2,5,6,7,3],2,3,4]]
+// let solve_Array = another_array.flat(Infinity)
+// console.log(solve_Array);
 
  
 
+const myArr1 = ["naresh ","Aditya","Sourabh","Deepak"]
+const myArr2 = ["Singh","Yadav","Vasihtha","Singh"]
 
-
-
+const join = myArr1.concat(myArr2)
+console.log(join);
