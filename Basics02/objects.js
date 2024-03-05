8acc54911c8000781ad3bc4c8bb12011b556827a
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2024-03-05 17:52:46 +0000

    changes in object

--- Basics02/objects.js
+++ Basics02/objects.js
@@ -6,6 +6,15 @@
 // }
 // // console.log(obj1)
 
+
+const newobj = {
+    name : "Naresh singh",
+    Add  : "Bajrang colony Ashta",
+    Email : "naresh.s.ashta@gmail.com",
+    Inst : "VIT BHOPAL",
+}
+console.log(newobj["Inst"]);
+
 // const obj2 = {
 //     name : "kuch bhi",
 //     age : 10,
@@ -15,22 +24,22 @@
 
 // Assign 
 
-const obj1 = {1:"a",2:"b",3:"c"}
-const obj2 = {4:"d",5:"e",6:"f"}
-const obj3 = Object.assign(obj1,obj2)
-console.log(obj3);
+// const obj1 = {1:"a",2:"b",3:"c"}
+// const obj2 = {4:"d",5:"e",6:"f"}
+// const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3);
 
 // Spread
 
-const obj4 = {...obj1,...obj2}
-console.log(obj4);
+// const obj4 = {...obj1,...obj2}
+// console.log(obj4);
 
-const course = {
-    coursename : "data Science ",
-    price : "999",
-    corseinstructor : "vaira" ,
-}
+// const course = {
+//     coursename : "data Science ",
+//     price : "999",
+//     corseinstructor : "vaira" ,
+// }
 
-const {corseinstructor} = course
-console.log(corseinstructor);
+// const {corseinstructor} = course
+// console.log(corseinstructor);
 
