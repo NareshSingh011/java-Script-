a2d49f07f2a1eabfa21ea23970af25bcb772bc7c
Naresh Singh <114250112+NareshSingh011@users.noreply.github.com>
2024-03-11 19:10:13 +0000

    changes in function

--- Basics03/function.js
+++ Basics03/function.js
@@ -1,31 +1,76 @@
 // Rest operator 
-function createcart(...num1){
-    return num1
+// function createcart(...num1){
+//     return num1
+// }
+// console.log(createcart(200,300,400))
+
+// ... inn dots ka use bahut sare number ko lene k liya hua hai 
+// function createcart(...nums1) {
+//     return nums1
+// }
+// console.log(createcart(200,300,2000,200));
+
+
+// // Object
+
+// const usser = {
+//     name : "naresh Singh",
+//     add : "Bajrang colony ashta",
+// }
+
+// function createcart(any){
+//     console.log(`my name is ${any.name} and my add is ${any.add}`);
+// }
+// createcart({
+//     name : "Deepak"
+// })
+
+
+// const user = {
+//     name : "Naresh Singh ",
+//     Email : "naresh.s.ashta@gamil.com",
+//     add : "Bajrang colony ashta"
+// }
+// function createcart(e) {
+//     console.log(`this is my name ${e.name}and this is my add ${e.add}`);
+// }
+// createcart({
+//     name:"Deepak singh"
+// })
+
+
+// // Array 
+
+// const myarray = [200,300,400,500]
+
+// function second(Array){
+//     return Array[1]
+// }
+// console.log(second(myarray))
+// //naresh 
+
+
+// const array = [300,400,500,600]
+
+// function second(nar) {
+//     return array[0]
+// }
+// console.log(second(array));
+
+// Add two numbers 
+
+// function addTwoNumbers(number1,number2) {
+//     console.log(number1+number2);
+// }
+
+// addTwoNumbers(10,10)
+
+
+function addTwoNumbers(number1,number2) {
+    let result = number1+number2
+    console.log("naresh Singh ");
+    return result
 }
-console.log(createcart(200,300,400))
 
-// Object
-
-const usser = {
-    name : "naresh Singh",
-    add : "Bajrang colony ashta",
-}
-
-function createcart(any){
-    console.log(`my name is ${any.name} and my add is ${any.add}`);
-}
-createcart({
-    name : "Deepak"
-})
-
-// Array 
-
-const myarray = [200,300,400,500]
-
-function second(Array){
-    return Array[1]
-}
-console.log(second(myarray))
-//naresh 
-
-
+const result = addTwoNumbers(10,10)
+console.log("Result:", (result));
